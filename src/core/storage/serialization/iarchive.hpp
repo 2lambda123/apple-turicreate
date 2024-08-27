426871bf469f2547402738e165c5dce0d2d11c6d
Restyled.io <commits@restyled.io>
2024-08-27 17:49:33 +0000

    Restyled by astyle

--- src/core/storage/serialization/iarchive.hpp
+++ src/core/storage/serialization/iarchive.hpp
@@ -19,46 +19,46 @@
 #include <core/storage/serialization/dir_archive.hpp>
 namespace turi {
 
-  /**
-   * \ingroup group_serialization
-   * \brief The serialization input archive object which, provided
-   * with a reference to an istream, will read from the istream,
-   * providing deserialization capabilities.
-   *
-   * Given a source of serialized bytes (written by an turi::oarchive),
-   * in the form of a standard input stream, you can construct an iarchive
-   * object by:
-   * \code
-   *   // where strm is an istream object
-   *   turi::iarchive iarc(strm);
-   * \endcode
-   *
-   * For instance, to deserialize from a file,
-   * \code
-   *   std::ifstream fin("inputfile.bin");
-   *   turi::iarchive iarc(fin);
-   * \endcode
-   *
-   * Once the iarc object is constructed, \ref sec_serializable
-   * objects can be read from it using the >> stream operator.
-   *
-   * \code
-   *    iarc >> a >> b >> c;
-   * \endcode
-   *
-   * Alternatively, data can be directly read from the stream using
-   * the iarchive::read() and iarchive::read_char() functions.
-   *
-   * For more usage details, see \ref serialization
-   *
-   * The iarchive object should not be used once the associated stream
-   * object is closed or is destroyed.
-   *
-   * To use this class, include
-   * core/storage/serialization/serialization_includes.hpp
-   */
-  class iarchive {
-  public:
+/**
+ * \ingroup group_serialization
+ * \brief The serialization input archive object which, provided
+ * with a reference to an istream, will read from the istream,
+ * providing deserialization capabilities.
+ *
+ * Given a source of serialized bytes (written by an turi::oarchive),
+ * in the form of a standard input stream, you can construct an iarchive
+ * object by:
+ * \code
+ *   // where strm is an istream object
+ *   turi::iarchive iarc(strm);
+ * \endcode
+ *
+ * For instance, to deserialize from a file,
+ * \code
+ *   std::ifstream fin("inputfile.bin");
+ *   turi::iarchive iarc(fin);
+ * \endcode
+ *
+ * Once the iarc object is constructed, \ref sec_serializable
+ * objects can be read from it using the >> stream operator.
+ *
+ * \code
+ *    iarc >> a >> b >> c;
+ * \endcode
+ *
+ * Alternatively, data can be directly read from the stream using
+ * the iarchive::read() and iarchive::read_char() functions.
+ *
+ * For more usage details, see \ref serialization
+ *
+ * The iarchive object should not be used once the associated stream
+ * object is closed or is destroyed.
+ *
+ * To use this class, include
+ * core/storage/serialization/serialization_includes.hpp
+ */
+class iarchive {
+public:
     std::istream* in = NULL;
     dir_archive* dir = NULL;
     const char* buf = NULL;
@@ -73,27 +73,27 @@ namespace turi {
      * assiciated input stream.
      */
     inline iarchive(std::istream& instream)
-      : in(&instream) { }
+        : in(&instream) { }
 
     inline iarchive(const char* buf, size_t len)
-      : buf(buf), off(0), len(len) { }
+        : buf(buf), off(0), len(len) { }
 
 
     inline iarchive(dir_archive& dirarc)
-      : in(dirarc.get_input_stream()),dir(&dirarc) {}
+        : in(dirarc.get_input_stream()),dir(&dirarc) {}
 
     ~iarchive() {}
 
     /// Directly reads a single character from the input stream
     inline char read_char() {
-      char c;
-      if (buf) {
-        c = buf[off];
-        ++off;
-      } else {
-        in->get(c);
-      }
-      return c;
+        char c;
+        if (buf) {
+            c = buf[off];
+            ++off;
+        } else {
+            in->get(c);
+        }
+        return c;
     }
 
     /**
@@ -101,12 +101,12 @@ namespace turi {
      *  input stream into the location pointed to by "c"
      */
     inline void read(char* c, size_t l) {
-      if (buf) {
-        memcpy(c, buf + off, l);
-        off += l;
-      } else {
-        in->read(c, l);
-      }
+        if (buf) {
+            memcpy(c, buf + off, l);
+            off += l;
+        } else {
+            in->read(c, l);
+        }
     }
 
 
@@ -116,43 +116,43 @@ namespace turi {
      */
     template <typename T>
     inline void read_into(T& c) {
-      if (buf) {
-        memcpy(reinterpret_cast<void*>(&c), buf + off, sizeof(T));
-        off += sizeof(T);
-      } else {
-        in->read(reinterpret_cast<char*>(&c), sizeof(T));
-      }
+        if (buf) {
+            memcpy(reinterpret_cast<void*>(&c), buf + off, sizeof(T));
+            off += sizeof(T);
+        } else {
+            in->read(reinterpret_cast<char*>(&c), sizeof(T));
+        }
     }
 
     /// Returns true if the underlying stream is in a failure state
     inline bool fail() {
-      return in == NULL ? off > len : in->fail();
+        return in == NULL ? off > len : in->fail();
     }
 
     std::string get_prefix() {
-      ASSERT_NE(dir, NULL);
-      return dir->get_next_read_prefix();
+        ASSERT_NE(dir, NULL);
+        return dir->get_next_read_prefix();
     }
-  };
+};
 
 
-  /**
-   * \ingroup group_serialization
-   * \brief
-   * When this archive is used to deserialize an object,
-   * and the object does not support serialization,
-   * failure will only occur at runtime. Otherwise equivalent to
-   * turi::iarchive.
-   */
-  class iarchive_soft_fail{
-  public:
+/**
+ * \ingroup group_serialization
+ * \brief
+ * When this archive is used to deserialize an object,
+ * and the object does not support serialization,
+ * failure will only occur at runtime. Otherwise equivalent to
+ * turi::iarchive.
+ */
+class iarchive_soft_fail {
+public:
 
     iarchive *iarc;
     bool mine;
 
     /// Directly reads a single character from the input stream
     inline char read_char() {
-      return iarc->read_char();
+        return iarc->read_char();
     }
 
     /**
@@ -160,7 +160,7 @@ namespace turi {
      *  input stream into the location pointed to by "c"
      */
     inline void read(char* c, size_t len) {
-      iarc->read(c, len);
+        iarc->read(c, len);
     }
 
     /**
@@ -169,18 +169,18 @@ namespace turi {
      */
     template <typename T>
     inline void read_into(T& c) {
-      iarc->read_into(c);
+        iarc->read_into(c);
     }
 
 
     /// Returns true if the underlying stream is in a failure state
     inline bool fail() {
-      return iarc->fail();
+        return iarc->fail();
     }
 
 
     std::string get_prefix() {
-      return iarc->get_prefix();
+        return iarc->get_prefix();
     }
 
     /**
@@ -190,127 +190,129 @@ namespace turi {
      * assiciated input stream.
      */
     inline iarchive_soft_fail(std::istream &instream)
-      : iarc(new iarchive(instream)), mine(true) {}
+        : iarc(new iarchive(instream)), mine(true) {}
 
     /**
      * Constructs an iarchive_soft_fail object from an iarchive.
      * Both will share the same input stream
      */
     inline iarchive_soft_fail(iarchive &iarc)
-      : iarc(&iarc), mine(false) {}
+        : iarc(&iarc), mine(false) {}
 
-    inline ~iarchive_soft_fail() { if (mine) delete iarc; }
-  };
+    inline ~iarchive_soft_fail() {
+        if (mine) delete iarc;
+    }
+};
 
 
-  namespace archive_detail {
+namespace archive_detail {
 
-    /// called by the regular archive The regular archive will do a hard fail
-    template <typename InArcType, typename T>
-    struct deserialize_hard_or_soft_fail {
-      inline static void exec(InArcType& iarc, T& t) {
+/// called by the regular archive The regular archive will do a hard fail
+template <typename InArcType, typename T>
+struct deserialize_hard_or_soft_fail {
+    inline static void exec(InArcType& iarc, T& t) {
         t.load(iarc);
-      }
-    };
+    }
+};
 
-    /// called by the soft fail archive
-    template <typename T>
-    struct deserialize_hard_or_soft_fail<iarchive_soft_fail, T> {
-      inline static void exec(iarchive_soft_fail& iarc, T& t) {
+/// called by the soft fail archive
+template <typename T>
+struct deserialize_hard_or_soft_fail<iarchive_soft_fail, T> {
+    inline static void exec(iarchive_soft_fail& iarc, T& t) {
         load_or_fail(*(iarc.iarc), t);
-      }
-    };
+    }
+};
 
 
-    /**
-       Implementation of the deserializer for different types.  This is the
-       catch-all. If it gets here, it must be a non-POD and is a class.  We
-       therefore call the .save function.  Here we pick between the archive
-       types using serialize_hard_or_soft_fail
-    */
-    template <typename InArcType, typename T, bool IsPOD, typename Enable = void>
-    struct deserialize_impl {
-      inline static void exec(InArcType& iarc, T& t) {
+/**
+   Implementation of the deserializer for different types.  This is the
+   catch-all. If it gets here, it must be a non-POD and is a class.  We
+   therefore call the .save function.  Here we pick between the archive
+   types using serialize_hard_or_soft_fail
+*/
+template <typename InArcType, typename T, bool IsPOD, typename Enable = void>
+struct deserialize_impl {
+    inline static void exec(InArcType& iarc, T& t) {
         deserialize_hard_or_soft_fail<InArcType, T>::exec(iarc, t);
-      }
-    };
+    }
+};
 
-    // catch if type is a POD
-    template <typename InArcType, typename T>
-    struct deserialize_impl<InArcType, T, true>{
-      inline static void exec(InArcType& iarc, T &t) {
+// catch if type is a POD
+template <typename InArcType, typename T>
+struct deserialize_impl<InArcType, T, true> {
+    inline static void exec(InArcType& iarc, T &t) {
         iarc.read_into(t);
-      }
-    };
+    }
+};
 
-  } //namespace archive_detail
+} //namespace archive_detail
 
-  /// \cond TURI_INTERNAL
+/// \cond TURI_INTERNAL
 
-  /**
-     Allows Use of the "stream" syntax for serialization
-  */
-  template <typename T>
-  inline iarchive& operator>>(iarchive& iarc, T &t) {
+/**
+   Allows Use of the "stream" syntax for serialization
+*/
+template <typename T>
+inline iarchive& operator>>(iarchive& iarc, T &t) {
     archive_detail::deserialize_impl<iarchive,
-                                     T,
-                                     gl_is_pod<T>::value >::exec(iarc, t);
+                   T,
+                   gl_is_pod<T>::value >::exec(iarc, t);
     return iarc;
-  }
+}
 
 
 
-  /**
-     Allows Use of the "stream" syntax for serialization
-  */
-  template <typename T>
-  inline iarchive_soft_fail& operator>>(iarchive_soft_fail& iarc, T &t) {
+/**
+   Allows Use of the "stream" syntax for serialization
+*/
+template <typename T>
+inline iarchive_soft_fail& operator>>(iarchive_soft_fail& iarc, T &t) {
     archive_detail::deserialize_impl<iarchive_soft_fail,
-                                     T,
-                                     gl_is_pod<T>::value >::exec(iarc, t);
+                   T,
+                   gl_is_pod<T>::value >::exec(iarc, t);
     return iarc;
-  }
+}
 
 
-  /**
-     deserializes an arbitrary pointer + length from an archive
-  */
-  inline iarchive& deserialize(iarchive& iarc,
-                               void* str,
-                               const size_t length) {
+/**
+   deserializes an arbitrary pointer + length from an archive
+*/
+inline iarchive& deserialize(iarchive& iarc,
+                             void* str,
+                             const size_t length) {
     iarc.read(reinterpret_cast<char*>(str), (std::streamsize)length);
     assert(!iarc.fail());
     return iarc;
-  }
+}
 
 
 
-  /**
-     deserializes an arbitrary pointer + length from an archive
-  */
-  inline iarchive_soft_fail& deserialize(iarchive_soft_fail& iarc,
-                                         void* str,
-                                         const size_t length) {
+/**
+   deserializes an arbitrary pointer + length from an archive
+*/
+inline iarchive_soft_fail& deserialize(iarchive_soft_fail& iarc,
+                                       void* str,
+                                       const size_t length) {
     iarc.read(reinterpret_cast<char*>(str), (std::streamsize)length);
     assert(!iarc.fail());
     return iarc;
-  }
+}
 
-  /// \endcond TURI_INTERNAL
+/// \endcond TURI_INTERNAL
 
-  /**
-     \ingroup group_serialization
+/**
+   \ingroup group_serialization
 
-     \brief Macro to make it easy to define out-of-place loads
+   \brief Macro to make it easy to define out-of-place loads
 
-     In the event that it is impractical to implement a save() and load()
-     function in the class one wnats to serialize, it is necessary to define
-     an "out of save" save and load.
+   In the event that it is impractical to implement a save() and load()
+   function in the class one wnats to serialize, it is necessary to define
+   an "out of save" save and load.
 
-     See \ref sec_serializable_out_of_place for an example
+   See \ref sec_serializable_out_of_place for an example
 
-     \note important! this must be defined in the global namespace!
-  */
+   \note important! this must be defined in the global namespace!
+*/
 #define BEGIN_OUT_OF_PLACE_LOAD(arc, tname, tval)       \
   namespace turi{ namespace archive_detail {        \
   template <typename InArcType>                           \
